8f31988096fb9bbbf47df5786f763861fe7a60bf
Edward Oliveros <125221155+EdwardOliveros@users.noreply.github.com>
2023-02-27 23:50:15 -0500

    Update Except_1.py

--- Febrero/Comentarios_Except/Except_1.py
+++ Febrero/Comentarios_Except/Except_1.py
@@ -1,5 +1,5 @@
-try:                             #Inicializa la función que permitira intentar y evaluar una operación por medio de unas instrucciones
-    #print(1/1))                 #Imprime la division de los valores insertados 
-    raise SyntaxError            #la funcion "raise" permite ejecutar y arrojar la excepcion sin necesidad de evaluar que se ejecuten intrucciones    
-except SyntaxError:              #con la funcion "except" se especificara el tipo de error se debera validar para arrojar un respuesta a continuacion
-    print('Cierra el parentesis')#se invoca la funcion "print" para imprimir por pantalla lo que se indique una vez ocurra la funcion "except"
+try:                              #Inicializa la función denominada "try" que cumple con tiene como facultad intentar y evaluar operaciones por medio de una serie de instrucciones.
+    #print(1/1))                  #Linea de codigo Comentada emplea una funcion print para imprimir la division de los valores insertados, se encuentra identada al bloque "try" para ser tomada en medio de la ejecucion del mismo 
+    raise SyntaxError             #la funcion "raise" permite ejecutar y arrojar la excepcion sin necesidad de evaluar que se ejecuten intrucciones, para este caso ejecuta un tipo de error que sera evaluado de tipo sintatico.
+except SyntaxError:               #con la funcion "except" se especificara el tipo de error y se debera validar para arrojar un respuesta a continuacion, se encuentra fuera del bloque "try" siendo un fragmento clave para excluir el error ejecutado.
+    print('Cierra el parentesis') #se invoca la funcion "print" dentro del bloque "except" para imprimir por pantalla lo que se indique una vez ocurra la funcion "except".
