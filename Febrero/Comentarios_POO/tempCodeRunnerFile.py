1541b45d81b6449be7dc298a2212fbb1addd55a5
Edward Oliveros <125221155+EdwardOliveros@users.noreply.github.com>
2023-02-28 08:05:15 -0500

    Create tempCodeRunnerFile.py

---
+++ Febrero/Comentarios_POO/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+cad='Amo la programación'   #Creación de variable con nombre "cad" y asignación de cadena de texto con mensaje.
+print(type(cad))            #Uso de la función print para mostrar por pantalla el tipo de dato al que pertenece la variable "cad" usando la función "type" para identificar el tipo de dato.
