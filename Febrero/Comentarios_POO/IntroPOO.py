780bd32d21dfbe2e2249fdfe00b4abbe94c384d9
Edward Oliveros <125221155+EdwardOliveros@users.noreply.github.com>
2023-02-28 08:02:56 -0500

    Create IntroPOO.py

---
+++ Febrero/Comentarios_POO/IntroPOO.py
@@ -0,0 +1,7 @@
+#NO tipado                #Linea de codigo comentada, muestra un mensaje "NO tipado".
+x=10                      #Creación de varible "x" a la cual se le asignara por medio del operador "=" el numero entero "10".
+x=[]                      #Asignacion de una lista vacia a la variable "x".
+cad='Amo la programación' #Creación de la variable "cad" para que se le asigne un valor de cadena de texto el cual es "Amo la programación".
+print(type(cad))          #Uso de la función print para mostrar por pantalla el tipo de dato al que pertenece la variable "cad" usando la función "type" para identificar el tipo de dato.
+print(len(cad))           #Impresión de la función "len" devuelve un valor entero que indica la cantidad de caracteres en la cadena de entrada "cad". 
+print(cad.capitalize())   #Impresión de la varible cad seguida de el método capitalize() que convierte el primer carácter de una cadena en una letra mayúscula y todos los demás alfabetos en minúsculas.
