8b8a161b971603856cf625271e12781e14638c9d
Edward_Oliveros <esoliveros07@soy.sena.edu.co>
2023-02-28 15:59:01 -0500

    Primer commit

---
+++ principal.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+        <div class="AREA_IZQ">
+            <img src="imgs/php_logo.png" alt="imagen cabecera izquierda" width="350" height="180">
+        </div>
+
+
+        <div class="AREA_CEN">
+            <div class="titulo">
+                <strong>
+                     <center> EMPRESA SOCIAL DE SOACHA </center>
+                </strong>
+            </div>
+            
+            <div class="enunciado">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum optio alias dolore, commodi minima veniam deserunt maiores, nisi vel ea deleniti quos, non laboriosam voluptas aut reprehenderit facere! Unde, exercitationem!
+            </div>
+        </div>
+
+
+        <div class="AREA_DER">
+            <img src="imgs/diagrama.png" alt="imagen cabecera derecha" width="350" height="180">
+        </div>
+
+
+
+    </header>
+
+    <NAV>
+        <div class=content_nav>
+
+        <div class="img_nav"> 
+            <img src="imgs/buenas-practicas-html.png" alt = "imagen navegador">
+
+        </div>
+
+        <ul>
+            <li><a href="Pruebas/inicio.php">Inicio</a></li>
+            <li><a href="compras.php">Compras</a></li>
+            <li><a href="quienes_somos.php">Quines Somos</a></li>
+            <li><a href="pqrs.php">PQRS</a></li>
+            <li><a href="facturacion.php">Facturación</a></li>
+        </ul>
+
+        </div>
+        
+
+    </NAV>
+
+    
+    <SECTION></SECTION>
+    <ASIde></ASIde>
+    <FOOter></FOOter>
+
+</body>
+</html>
