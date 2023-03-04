4e12f651e15165cae6aafb7e3e05fb3e906e0eb4
Edward Oliveros <125221155+EdwardOliveros@users.noreply.github.com>
2023-03-04 12:33:58 -0500

    Create IntroHerencia.py

---
+++ Marzo/Herencia/Comentarios/IntroHerencia.py
@@ -0,0 +1,7 @@
+def suma (a,b):     #Se define la funcion "suma" con dos parametros "a" y "b"
+    return a+b      #se retornan dichos valores con una operacion de suma
+
+def suma(a,b,c):    #definicion de funcion con tres parametros "a", "b" y "c"
+    return a+b+c    #retorno de los valores sumados entre si
+
+print(suma(1,2,3))  #Impresion de la funcion con asignacion de valores enteros.
